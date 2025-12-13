0fac5c377b51966a5afee2a9f5230bbc6ecbc303
nik26788 <risinnshow@gmail.com>
2025-12-13 16:10:47 +0800

    feat(browser): ✨  batch send

--- packages/browser/src/transport/index.ts
+++ packages/browser/src/transport/index.ts
@@ -21,3 +21,40 @@ export class BrowserTransport implements Transport {
         }).catch(err => console.error('Failed to send data', err))
     }
 }
+
+/**
+ * implement based on count and period transport
+ */
+
+// export class BrowserTransport implements Transport {
+//     constructor(private dsn: string) {}
+//
+//     private batchSize = 10
+//     private batch: Record<string, unknown>[] = []
+//
+//     send(data: Record<string, unknown>) {
+//         const browserInfo = getBrowserInfo()
+//
+//         const payload = {
+//             ...data,
+//             browserInfo,
+//         }
+//
+//         if (this.batch.length >= this.batchSize) {
+//             this.flush()
+//         }
+//     }
+//
+//     flush() {
+//         const batchItems = this.batch.slice(0, this.batchSize)
+//         this.batch = this.batch.slice(this.batchSize)
+//
+//         fetch(this.dsn, {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify(batchItems),
+//         }).catch(err => console.error('Failed to send data', err))
+//     }
+// }
